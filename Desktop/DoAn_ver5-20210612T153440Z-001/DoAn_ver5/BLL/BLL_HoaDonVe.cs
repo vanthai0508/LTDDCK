dd62c1aa0a174a28a64565d2868848d2557b0ed7
ttb2k1 <ttb2k1@gmail.com>
2021-07-24 16:44:25 +0700

    Add BLL_HoaDonVe

---
+++ Desktop/DoAn_ver5-20210612T153440Z-001/DoAn_ver5/BLL/BLL_HoaDonVe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoAn_ver5.DTO;
+using DoAn_ver5.DAL;
+
+namespace DoAn_ver5.BLL
+{
+    class BLL_HoaDonVe
+    {
+        private static BLL_HoaDonVe _Instance;
+        public static BLL_HoaDonVe Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new BLL_HoaDonVe();
+                }
+                return _Instance;
+            }
+            private set => _Instance = value;
+        }
+        public List<DTO_HoaDonVe> GetListHoaDonVe()
+        {
+            List<DTO_HoaDonVe> list = new List<DTO_HoaDonVe>();
+            foreach(DataRow row in DAL_HoaDonVe.Instance.GetAllHoaDonVe().Rows)
+            {
+                list.Add(new DTO_HoaDonVe(row["MaHoaDonVe"].ToString(), row["MaSuatChieu"].ToString(), row["MaGhe"].ToString(), (DateTime)row["NgayBanVe"], row["MaNhanVien"].ToString(), (float)row["GiaVe"], row["MaKhachHang"].ToString());
+            }
+            return list;    
+        }
+    }
+}
